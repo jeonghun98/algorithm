08de78f40038e3d9dd9ee4f6ff42b4f4636a002e
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2022-10-24 20:26:53 +0900

    다익스트라
    다익스트라

--- 1024/1024_2.py
+++ 1024/1024_2.py
@@ -26,7 +26,7 @@ def dijkstra(start) :
         # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
         if distance[now] < dist :
             continue
-            # 현재 노드와 연결된 다르 인접한 노드들을 확인
+            # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now] :
             cost = dist + i[1]
             # 현재 노드를 거쳐서, 다르 노드로 이동하는 거리가 더 짧은 경우 -> 리스트 갱신 후 큐에 삽입
