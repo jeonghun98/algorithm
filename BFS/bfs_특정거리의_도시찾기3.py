4b07243bf2dea848deac76d8601bdba1a5db5da5
JeongHun <wjdgns7040@gmail.com>
2023-06-01 14:23:13 +0900

    BFS
    BFS

---
+++ BFS/bfs_특정거리의_도시찾기3.py
@@ -0,0 +1,30 @@
+from collections import deque
+# 입력 시간 줄이기
+import sys
+input = sys.stdin.readline
+
+n, m, k, x = map(int, input().split())
+data = [[] for _ in range(n+1)]
+dis = [-1] * (n+1)
+for _ in range(m) :
+    s, e = map(int, input().split())
+    data[s].append(e)
+
+def bfs(start) :
+    dis[start] = 0
+    q = deque()
+    q.append(start)
+    while q :
+        now = q.popleft()
+        for i in data[now] :
+            if dis[i] == -1:
+                q.append(i)
+                dis[i] = dis[now] + 1
+
+bfs(x)
+
+if dis.count(k) == 0 :
+    print(-1)
+else :
+    for i,d in enumerate(dis) :
+        if d == k: print(i)
